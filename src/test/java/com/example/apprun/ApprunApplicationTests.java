15ef1886713a7d86fc59e4464650d11d0a6acdab
Ana Jimenez <anajimenez.tw@packlink.com>
2021-08-04 09:20:12 +0200

    bootstraping

---
+++ src/test/java/com/example/apprun/ApprunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apprun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
